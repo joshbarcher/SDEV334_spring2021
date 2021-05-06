d17f4e06277a6f98698dcd4d47b635d408968618
Josh Archer <jarcher@greenriver.edu>
2021-05-06 12:02:07 -0700

    Added methods for generating random arrays and detecting inversions in an array.

--- sorting/src/sorts/MergeSort.java
+++ sorting/src/sorts/MergeSort.java
@@ -1,12 +1,53 @@
 package sorts;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class MergeSort
 {
     private static int[] aux;
 
     public static void main(String[] args)
     {
+        int[] array = genArray(10);
 
+        //unsorted
+        System.out.println(Arrays.toString(array));
+        System.out.println("Detected inversions: " +
+                detectAdjacentInversions(array));
+        System.out.println();
+
+        //sorted
+        mergeSort(array);
+        System.out.println(Arrays.toString(array));
+        System.out.println("Detected inversions: " +
+                detectAdjacentInversions(array));
+    }
+
+    private static boolean detectAdjacentInversions(int[] array)
+    {
+        //check whether any two adjacent elements are out of order
+        for (int i = 0; i < array.length - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                return true;
+            }
+        }
+        return false; //non were found, the array is sorted!
+    }
+
+    private static int[] genArray(int size)
+    {
+        int[] results = new int[size];
+
+        //put random numbers in the array
+        for (int i = 0; i < results.length; i++)
+        {
+            results[i] = (int)Math.ceil(Math.random() * size);
+        }
+
+        return results;
     }
 
     public static void mergeSort(int[] array)
@@ -23,15 +64,18 @@ public class MergeSort
 
     private static void mergeSort(int[] array, int low, int high)
     {
+        //there are no elements between low and high
         if (high - low <= 0)
         {
             return;
         }
 
+        //otherwise, find the mid point and recusively divide our array
         int mid = (high + low) / 2;
         mergeSort(array, low, mid);
         mergeSort(array, mid + 1, high);
 
+        //after we have sorted the left and right sub-array, then merge them
         merge(array, low, high);
     }
 
@@ -44,6 +88,7 @@ public class MergeSort
 
         for (int i = 0; i < numToMerge; i++)
         {
+            //have I exhausted all elements in the left or right sub-array?
             if (left > mid)
             {
                 aux[i] = array[right++];
@@ -52,6 +97,7 @@ public class MergeSort
             {
                 aux[i] = array[left++];
             }
+            //pick the smallest element
             else if (array[left] < array[right])
             {
                 aux[i] = array[left++];
